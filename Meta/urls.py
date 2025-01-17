780a5c0df37a1dcba4de72b24248e609906e54f8
jithin <jithinjzx@gmail.com>
2025-01-17 13:06:38 +0530

    urls

--- Meta/urls.py
+++ Meta/urls.py
@@ -1,17 +1,20 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('checkin/', include('checkin.urls')),
-    path('leave/', include('leave.urls')),
+    path('', include('home.urls')), 
+    path('leave/', include('leave.urls')), 
     path('profiles/', include('profiles.urls')),
-    path('admins/', include('admins.urls')),
+    path('admins/', include('admins.urls')), 
+    path('checkin/', include('checkin.urls')),
     path('assign/', include('assign.urls')),
+    
     
 ]
 
-from django.urls import include
-urlpatterns += [path('', include('account.urls'))]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
