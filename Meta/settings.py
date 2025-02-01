526696b22cf8574ce077e5749ae3bafe6668559d
jithin <jithinjzx@gmail.com>
2025-02-01 19:08:08 +0530

    profile update

--- Meta/settings.py
+++ Meta/settings.py
@@ -8,7 +8,11 @@ SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='.vercel.app,.now.sh,localhost,127.0.0.1',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
 
 
 # Application definition
